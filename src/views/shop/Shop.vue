1b18ee276f48b08232521c8a45ad3a2dede37345
郝雷 <m13522389185@163.com>
2021-03-05 23:45:46 +0800

    Shop component

--- src/views/shop/Shop.vue
+++ src/views/shop/Shop.vue
@@ -1,8 +1,32 @@
 <template>
-  <div>jhbjhb</div>
+  <div class="wrapper">
+
+    <ShopInfo :item="item" />
+  </div>
 </template>
 <script>
-export default {}
+import ShopInfo from '../../components/ShopInfo'
+export default {
+  name: 'Shop',
+  components: { ShopInfo },
+  setup() {
+    const item = {
+      _id: 1,
+      name: '沃尔玛1',
+      imgUrl: 'http://www.dell-lee.com/imgs/vue3/near.png',
+      sales: 10000,
+      expressLimit: 0,
+      expressPrice: 5,
+      slogan: 'VIP尊享满89减少4元'
+    }
+    return {
+      item
+    }
+  }
+}
 </script>
 <style scoped>
+.wrapper {
+  padding: 0 0.18rem;
+}
 </style>
